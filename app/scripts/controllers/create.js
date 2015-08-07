7552b1988c7044cbfbca579f6365d6cc7dacb084
emazurek <Edward.R.Mazurek@gmail.com>
2015-08-07 17:06:29 -0400

    simple create tree, addSong not yet working

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -7,21 +7,35 @@
  * A demo of using AngularFire to manage a synchronized list.
  */
 angular.module('musicsyncApp')
-  .controller('CreateCtrl', function($scope, Ref, $firebaseArray, $timeout) {
-    $scope.model = $firebaseArray(Ref);   
-    console.log($scope.model);
+  .controller('CreateCtrl', function($scope, $log, Ref, $firebaseObject, $timeout) {
+    $scope.model = $firebaseObject(Ref);   
     $scope.model.$loaded().catch(alert);
+    $log.debug($scope.model);
+    $scope.newSong = {
+      'title': '',
+      'sections': []
+    };
+    
+    $scope.addNode = function(nodeType, sectionNo, partNo) {
+      if(nodeType === 'section') {
+        $scope.newSong.sections.push({'name': '', 'length': '', 'parts': []});
+      }
+      else if(nodeType === 'part') {
+        $scope.newSong.sections[sectionNo].parts.push({'name': '', 'instructions': []});
+      }
+      else if(nodeType === 'instruction') {
+        $scope.newSong.sections[sectionNo].parts[partNo].instructions.push({'content': '', 'type': ''});
+      }
+      $log.debug($scope.newSong);
+    };
 
     /* BELOW IS CODE */
-    $scope.newSong = {};
-    $scope.addSong = function(newSong) {
-          if( newSong ) {
-            // push a message to the end of the array
-            $scope.model.songs.push({
-              "title": $scope.newSong.title
-            });
-          }
-        };
+    
+    $scope.addSong = function() {
+        // push a message to the end of the array
+        $scope.model.songs.push($scope.newSong);
+        $scope.newSong = {};
+    };
 
     function alert(msg) {
       $scope.err = msg;
